238454197f94d5fdfe8395912f73213d5d9dc73c
calebwebster <calebwebsteredge@gmail.com>
2023-10-23 15:25:58 +1000

    Implement better-cond

--- src/tesla_solar_charger/implementations/target_time_regulation_creater.clj
+++ src/tesla_solar_charger/implementations/target_time_regulation_creater.clj
@@ -4,7 +4,8 @@
    [clojure.core.async :as async]
    [tesla-solar-charger.interfaces.site :as site]
    [tesla-solar-charger.interfaces.regulator :as regulator]
-   [tesla-solar-charger.time-utils :as time-utils]))
+   [tesla-solar-charger.utils :as utils]
+   [better-cond.core :refer [cond] :rename {cond better-cond}]))
 
 (defn did-car-start-charging-here?
   [current-state previous-regulation site]
@@ -40,7 +41,7 @@
   regulator/RegulationCreater
 
   (create-regulation [regulation-creater regulator car-state site-data]
-    (let [regulation (regulator/->ChargeRateChargeLimitRegulation car-state site-data)
+    (let [regulation (regulator/->ChargeRateRegulation car-state site-data)
           last-attempted-regulation (regulator/get-last-attempted-regulation regulator)
           last-successful-regulation (regulator/get-last-successful-regulation regulator)
           first-successful-regulation (regulator/get-first-successful-regulation regulator)
@@ -48,96 +49,106 @@
           settings (async/<!! settings-chan)]
       (when (nil? settings)
         (throw (ex-info "Channel closed" {})))
-      (cond
-        (and
-         (some? last-attempted-regulation)
-         (= (car/get-id car-state)
-            (car/get-id (regulator/get-car-state last-attempted-regulation))))
-        (-> regulation
-            (regulator/add-message (format "Already regulated this car state")))
+      (better-cond
+       (and
+        (some? last-attempted-regulation)
+        (= (car/get-id car-state)
+           (car/get-id (regulator/get-car-state last-attempted-regulation))))
+       (-> regulation
+           (regulator/add-message (format "Already regulated this car state")))
 
-        (did-car-start-charging-here? car-state last-attempted-regulation site)
-        (-> regulation
-            (regulator/set-did-car-start-charging true)
-            (regulator/set-charge-rate-amps 0)
-            (regulator/add-message (format "Car started charging")))
+       (did-car-start-charging-here? car-state last-attempted-regulation site)
+       (-> regulation
+           (regulator/set-did-car-start-charging true)
+           (regulator/set-charge-rate-amps 0)
+           (regulator/add-message (format "Car started charging")))
 
-        (did-car-stop-charging-here? car-state last-attempted-regulation site)
-        (let [charge-rate-amps (car/get-charge-rate-amps (regulator/get-car-state first-successful-regulation))]
-          (-> regulation
-              (regulator/set-did-car-stop-charging true)
-              (regulator/set-charge-rate-amps charge-rate-amps)
-              (regulator/add-message (format "Car stopped charging"))))
+       (did-car-stop-charging-here? car-state last-attempted-regulation site)
+       (let [charge-rate-amps (car/get-charge-rate-amps (regulator/get-car-state first-successful-regulation))]
+         (-> regulation
+             (regulator/set-did-car-stop-charging true)
+             (regulator/set-charge-rate-amps charge-rate-amps)
+             (regulator/add-message (format "Car stopped charging"))))
 
-        (not (site/is-car-here? site car-state))
-        (-> regulation
-            (regulator/add-message (format "Car is not at %s" (site/get-name site))))
+       (not (site/is-car-here? site car-state))
+       (-> regulation
+           (regulator/add-message (format "Car is not at %s" (site/get-name site))))
 
-        (not (car/is-charging? car-state))
-        (-> regulation
-            (regulator/add-message "Car is not charging"))
+       (not (car/is-charging? car-state))
+       (-> regulation
+           (regulator/add-message "Car is not charging"))
 
-        (car/is-override-active? car-state)
-        (-> regulation
-            (regulator/set-charge-rate-amps (car/get-max-charge-rate-amps car-state))
-            (regulator/add-message "Override active"))
+       (car/is-override-active? car-state)
+       (-> regulation
+           (regulator/set-charge-rate-amps (car/get-max-charge-rate-amps car-state))
+           (regulator/add-message "Override active"))
 
-        (and
-         (some? (:target-hour settings))
-         (some? (:target-minute settings))
-         (some? (:target-second settings))
-         (not (car/will-reach-target-by? car-state (:target-percent settings) (-> (java.time.LocalDateTime/now)
-                                                       (.withHour (:target-hour settings))
-                                                       (.withMinute (:target-minute settings))
-                                                       (.withSecond (:target-second settings))
-                                                       (.withNano 0)))))
-        (-> regulation
-            (regulator/set-charge-rate-amps (car/get-max-charge-rate-amps car-state))
-            (regulator/add-message (format "Overriding to reach %s%% by %s"
-                                           (car/get-charge-limit-percent car-state)
-                                           (time-utils/format-time "HH:mm:ss" (-> (java.time.LocalDateTime/now)
-                                                                                  (.withHour (:target-hour settings))
-                                                                                  (.withMinute (:target-minute settings))
-                                                                                  (.withSecond (:target-second settings))
-                                                                                  (.withNano 0))))))
+       :let [target-hour (:target-hour settings)
+             target-minute (:target-minute settings)
+             target-second (:target-second settings)
+             target-time (if (and
+                              (some? target-hour)
+                              (some? target-minute)
+                              (some? target-second))
+                           (-> (java.time.LocalDateTime/now)
+                               (.withHour target-hour)
+                               (.withMinute target-minute)
+                               (.withSecond target-second)
+                               (.withNano 0))
+                           nil)]
 
-        (nil? site-data)
-        (-> regulation
-            (regulator/add-message "No solar data"))
+       :let [target-percent (:target-percent settings)]
 
-        (and
-         (true? (regulator/used-solar-data? last-successful-regulation))
-         (= (site/get-time (last (site/get-points site-data)))
-            (site/get-time (last (site/get-points (regulator/get-site-data last-successful-regulation))))))
-        (-> regulation
-            (regulator/add-message "Already regulated this solar data"))
-        (nil? (site/get-excess-power-watts (last (site/get-points site-data))))
-        (-> regulation
-            (regulator/add-message "No excess power"))
+       (and
+        (some? target-time)
+        (some? target-percent)
+        (car/should-override-to-reach-target? car-state target-percent target-time))
+       (-> regulation
+           (regulator/set-charge-rate-amps (car/get-max-charge-rate-amps car-state))
+           (regulator/add-message (format "Overriding to reach %s%% by %s"
+                                          target-percent
+                                          (utils/format-time "HH:mm:ss" target-time))))
 
-        :else
-        (let [excess-power (site/get-excess-power-watts (last (site/get-points site-data)))
-              available-power-watts (- excess-power 1000)
-              current-rate-amps (car/get-charge-rate-amps car-state)
-              max-charge-rate-amps (car/get-max-charge-rate-amps car-state)
-              adjustment-rate-amps (site/power-watts-to-current-amps site available-power-watts)
-              adjustment-rate-amps (limit adjustment-rate-amps (- 8) 8)
-              new-charge-rate-amps (-> current-rate-amps
-                                       (+ adjustment-rate-amps)
-                                       float
-                                       Math/round
-                                       int
-                                       (limit 0 max-charge-rate-amps))]
-          (-> regulation
-              (regulator/set-used-solar-data true)
-              (regulator/set-charge-rate-amps new-charge-rate-amps)
-              (regulator/add-message (format "Excess power is %.2fW (%.2fW available)"
-                                             (float excess-power)
-                                             (float available-power-watts)))
-              (regulator/add-message (format "Altering charge rate to %sA (%s%s from %sA)"
-                                             new-charge-rate-amps
-                                             (if (neg? adjustment-rate-amps) "-" "+")
-                                             (abs (int adjustment-rate-amps))
-                                             current-rate-amps))))))))
+       (nil? site-data)
+       (-> regulation
+           (regulator/add-message "No solar data"))
+
+       (and
+        (true? (regulator/used-solar-data? last-successful-regulation))
+        (= (site/get-time (last (site/get-points site-data)))
+           (site/get-time (last (site/get-points (regulator/get-site-data last-successful-regulation))))))
+       (-> regulation
+           (regulator/set-used-solar-data true)
+           (regulator/add-message "Already regulated this solar data"))
+
+       (nil? (site/get-excess-power-watts (last (site/get-points site-data))))
+       (-> regulation
+           (regulator/add-message "No excess power"))
+
+       :else
+       (let [data-point (last (site/get-points site-data))
+             excess-power (site/get-excess-power-watts data-point)
+             available-power-watts (- excess-power 1000)
+             current-rate-amps (car/get-charge-rate-amps car-state)
+             max-charge-rate-amps (car/get-max-charge-rate-amps car-state)
+             adjustment-rate-amps (site/power-watts-to-current-amps site available-power-watts)
+             adjustment-rate-amps (limit adjustment-rate-amps (- 8) 8)
+             new-charge-rate-amps (-> current-rate-amps
+                                      (+ adjustment-rate-amps)
+                                      float
+                                      Math/round
+                                      int
+                                      (limit 0 max-charge-rate-amps))]
+         (-> regulation
+             (regulator/set-used-solar-data true)
+             (regulator/set-charge-rate-amps new-charge-rate-amps)
+             (regulator/add-message (format "Excess power is %.2fW (%.2fW available)"
+                                            (float excess-power)
+                                            (float available-power-watts)))
+             (regulator/add-message (format "Altering charge rate to %sA (%s%s from %sA)"
+                                            new-charge-rate-amps
+                                            (if (neg? adjustment-rate-amps) "-" "+")
+                                            (abs (int adjustment-rate-amps))
+                                            current-rate-amps))))))))
 
 
