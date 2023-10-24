83289211cd155a387a61182194d7c904cf1ed7a1
calebwebster <calebwebsteredge@gmail.com>
2023-10-24 11:10:49 +1000

    Add handling for blank and invalid settings filenames

--- src/tesla_solar_charger/gophers/provide_settings.clj
+++ src/tesla_solar_charger/gophers/provide_settings.clj
@@ -13,10 +13,14 @@
   [log-prefix settings-filename get-chan set-chan error-chan log-chan]
   (async/go
     (try
-      (loop [settings (try
-                        (json/parse-string (slurp settings-filename))
-                        (catch Exception e
-                          default-settings))]
+      (loop [settings (if (or
+                           (clojure.string/blank? settings-filename)
+                           (not (clojure.string/ends-with? ".json")))
+                        default-settings
+                        (try
+                          (json/parse-string (slurp settings-filename))
+                          (catch Exception e
+                            default-settings)))]
         (async/>! log-chan {:level :info :prefix log-prefix :message "..."})
         (let [[value chan] (async/alts! [[get-chan settings] set-chan])]
           (if (= get-chan chan)
@@ -31,7 +35,10 @@
               (let [new-settings (set-request settings)]
                 (async/>! log-chan {:level :info :prefix log-prefix :message "Received change request"})
                 (async/>! log-chan {:level :verbose :prefix log-prefix :message new-settings})
-                (spit settings-filename (json/generate-string new-settings {:pretty true}))
+                (when (and
+                       (not (clojure.string/blank? settings-filename))
+                       (clojure.string/ends-with? ".json"))
+                  (spit settings-filename (json/generate-string new-settings {:pretty true})))
                 (recur new-settings))))))
       (catch clojure.lang.ExceptionInfo e
         (async/>! error-chan e))
