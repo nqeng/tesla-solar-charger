345971571f057babe2782be91598056b5b617eea
calebwebster <calebwebsteredge@gmail.com>
2024-07-24 22:19:42 +1000

    Move three phase functions to charger

--- src/tesla_solar_charger/implementations/site/three_phase_tesla_charger.clj
+++ src/tesla_solar_charger/implementations/site/three_phase_tesla_charger.clj
@@ -19,6 +19,26 @@
   (utils/throw-if-attribute-nil charger :max-current-amps)
   (:max-current-amps charger))
 
+(defn watts-to-amps-three-phase
+  [power-watts voltage-volts power-factor]
+  (/ power-watts (* voltage-volts power-factor (clojure.math/sqrt 3))))
+
+(defn amps-to-watts-three-phase
+  [current-amps voltage-volts power-factor]
+  (* voltage-volts current-amps power-factor (clojure.math/sqrt 3)))
+
+(defn watts-to-amps-three-phase-australia
+  [power-watts]
+  (let [voltage-volts 400
+        power-factor 1.0]
+    (watts-to-amps-three-phase power-watts voltage-volts power-factor)))
+
+(defn amps-to-watts-three-phase-australia
+  [current-amps]
+  (let [voltage-volts 400
+        power-factor 1.0]
+    (amps-to-watts-three-phase current-amps voltage-volts power-factor)))
+
 (defn convert-watts-to-amps [charger watts]
   (let [three-phase-voltage-volts (get-three-phase-voltage-volts charger)
         power-factor (get-power-factor charger)]
