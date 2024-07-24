be3517169898e22bcd5d8cc346288367b17bd038
calebwebster <calebwebsteredge@gmail.com>
2024-07-24 22:21:36 +1000

    Remove methods from charger interface

--- src/tesla_solar_charger/implementations/site/three_phase_tesla_charger.clj
+++ src/tesla_solar_charger/implementations/site/three_phase_tesla_charger.clj
@@ -53,26 +53,7 @@
   (set-car-charge-power [charger car power-watts]
     (let [current-amps (convert-watts-to-amps charger power-watts)
           result (Isite-charger/set-car-charge-current charger car current-amps)]
-      result))
-  (set-car-charge-current [charger car current-amps]
-    (let [charger-max-current-amps (get-max-current-amps charger)
-          current-amps (utils/clamp-min-max current-amps 0 charger-max-current-amps)]
-      (Icar/set-charge-current car current-amps)))
-  (get-car-max-charge-power-watts [charger car-state]
-    (let [max-charge-current-amps (Icar/get-max-charge-current-amps car-state)
-          max-charge-power-watts (convert-amps-to-watts charger max-charge-current-amps)]
-      max-charge-power-watts))
-  (get-car-charge-power-watts [charger car-state]
-    (let [charge-current-amps (Icar/get-charge-current-amps car-state)
-          charge-power-watts (convert-amps-to-watts charger charge-current-amps)]
-      charge-power-watts))
-  (adjust-car-charge-power [charger car power-adjustment-watts]
-    (let [car-state (Icar/get-state car)
-          charge-power-watts (Isite-charger/get-car-charge-power-watts charger car-state)
-          new-charge-power-watts (+ charge-power-watts power-adjustment-watts)]
-      (Isite-charger/set-car-charge-power charger car new-charge-power-watts)))
-  (is-car-connected-here? [charger car-state] (Icar/is-connected? car-state))
-  (is-car-charging-here? [charger car-state] (Icar/is-charging? car-state)))
+      result)))
 
 (defn new-TeslaChargerThreePhase
   [three-phase-voltage-volts max-current-amps power-factor]
