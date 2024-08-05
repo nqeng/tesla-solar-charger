c094c5efbab7b811a89f420366df7421a861a0a9
calebwebster <calebwebsteredge@gmail.com>
2024-08-05 14:22:33 +1000

    Remove charger

--- src/tesla_solar_charger/charger/charger.clj
+++
@@ -1,7 +0,0 @@
-(ns tesla-solar-charger.charger.charger)
-
-(defprotocol ICharger
-  (get-max-car-charge-power-watts [charger car-state])
-  (get-car-charge-power-watts [charger car-state])
-  (set-car-charge-power-watts [charger car power-watts]))
-
