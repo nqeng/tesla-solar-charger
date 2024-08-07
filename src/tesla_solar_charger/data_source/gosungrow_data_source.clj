fa46c4c780ed8275a8011042fd14872eaeecc8f6
calebwebster <calebwebsteredge@gmail.com>
2024-08-07 11:24:56 +1000

    Move GoSungrow config command to get data

--- src/tesla_solar_charger/data_source/gosungrow_data_source.clj
+++ src/tesla_solar_charger/data_source/gosungrow_data_source.clj
@@ -23,7 +23,7 @@
     rounded-time))
 
 (defn make-data-point-from-json
-  [object excess-power-key]
+  [object ps-point]
   (let [timestamp-formatter (java.time.format.DateTimeFormatter/ofPattern "yyyyMMddHHmmss")
         zone-id (java.time.ZoneId/of "Australia/Brisbane")
         timestamp (-> object
@@ -33,29 +33,69 @@
                       .toInstant)
         excess-power-watts (-> object
                                (get "points")
-                               (get excess-power-key)
+                               (get ps-point)
                                float
                                -)
         data-point (make-data-point timestamp excess-power-watts)]
     data-point))
 
+(defn execute-config-shell-command
+  [gosungrow-filepath appkey username password]
+  (sh 
+    gosungrow-filepath
+    "config" 
+    "write" 
+    (format "--appkey=%s" appkey) 
+    (format "--user=%s" username) 
+    (format "--password=%s" password)))
+
+(defn configure-gosungrow-executable
+  [gosungrow-filepath appkey username password]
+  (let [result (execute-config-shell-command gosungrow-filepath appkey username password)]
+    (when (not= 0 (:exit result))
+      (throw (ex-info 
+               (format "GoSungrow config write exited with code %d; %s %s" 
+                       (:exit result) 
+                       (:err result) 
+                       (:out result)) 
+               {})))))
+
 (defn execute-shell-command
-  [script-filepath ps-id ps-key excess-power-key minute-interval start-timestamp-string end-timestamp-string]
-  (let [result (sh
-                 script-filepath
-                 "data"
-                 "json"
-                 "AppService.queryMutiPointDataList" 
-                 (format "StartTimeStamp:%s" start-timestamp-string)
-                 (format "EndTimeStamp:%s" end-timestamp-string)
-                 (format "MinuteInterval:%s" (str minute-interval)) 
-                 (format "Points:%s" excess-power-key) 
-                 (format "PsId:%s" ps-id) 
-                 (format "PsKeys:%s" ps-key))] 
-    result))
+  [gosungrow-filepath ps-id ps-key ps-point data-interval-minutes start-timestamp-string end-timestamp-string]
+  (sh
+    gosungrow-filepath
+    "data"
+    "json"
+    "AppService.queryMutiPointDataList" 
+    (format "StartTimeStamp:%s" start-timestamp-string)
+    (format "EndTimeStamp:%s" end-timestamp-string)
+    (format "MinuteInterval:%s" (str data-interval-minutes)) 
+    (format "Points:%s" ps-point) 
+    (format "PsId:%s" ps-id) 
+    (format "PsKeys:%s" ps-key)))
+
+(defn get-gosungrow-json-data
+  [gosungrow-filepath ps-id ps-key ps-point data-interval-minutes start-timestamp-string end-timestamp-string]
+  (let [result (execute-shell-command 
+                 gosungrow-filepath
+                 ps-id
+                 ps-key
+                 ps-point
+                 data-interval-minutes
+                 start-timestamp-string
+                 end-timestamp-string)]
+    (when (not= 0 (:exit result))
+      (throw (ex-info 
+               (format "GoSungrow data json exited with code %d; %s %s" 
+                       (:exit result) 
+                       (:out result) 
+                       (:err result)) 
+               {})))
+    (json/parse-string (:out result))))
 
 (defn get-latest-data-point
-  [script-filepath gosungrow-appkey sungrow-username sungrow-password ps-key ps-id ps-point]
+  [gosungrow-filepath appkey username password ps-key ps-id ps-point]
+  (configure-gosungrow-executable gosungrow-filepath appkey username password)
   (let [time-now (java.time.Instant/now)
         data-interval-minutes 5
         zone-id (java.time.ZoneId/of "Australia/Brisbane")
@@ -71,80 +111,54 @@
                                    (round-minutes-down-to-interval data-interval-minutes)
                                    (.atZone zone-id)
                                    (.format timestamp-formatter))
-        result (execute-shell-command 
-                 script-filepath
-                 ps-id
-                 ps-key
-                 ps-point
-                 5
-                 start-timestamp-string
-                 end-timestamp-string)]
-    (when (not= 0 (:exit result))
-      (throw (ex-info (format "Failed to execute GoSungrow data command; %s %s" (:err result) (:out result)) {})))
-    (let [stdout (:out result)
-          json (json/parse-string stdout)
-          data (get json "data")
-          data-points (->> data
-                           (map second)
-                           (map #(make-data-point-from-json % ps-point)))
-          latest-data-point (->> data-points
-                                 (sort-by :timestamp)
-                                 last)]
-      (when (nil? latest-data-point)
-        (throw (ex-info "No data point found" {})))
-      latest-data-point)))
+        json-object (get-gosungrow-json-data
+                      gosungrow-filepath 
+                      ps-id 
+                      ps-key 
+                      ps-point 
+                      data-interval-minutes 
+                      start-timestamp-string 
+                      end-timestamp-string)
+        data (get json-object "data")
+        data-points (->> data
+                         (map second)
+                         (map #(make-data-point-from-json % ps-point)))
+        latest-data-point (->> data-points
+                               (sort-by :timestamp)
+                               last)]
+    (when (nil? latest-data-point)
+      (throw (ex-info "No data point found" {})))
+    latest-data-point))
 
 (defrecord GoSungrowDataSource []
   IDataSource
   (get-latest-data-point [data-source] 
-    (let [script-filepath (:script-filepath data-source)
-          gosungrow-appkey (:gosungrow-appkey data-source)
-          sungrow-username (:sungrow-username data-source)
-          sungrow-password (:sungrow-password data-source)
+    (let [gosungrow-filepath (:gosungrow-filepath data-source)
+          appkey (:appkey data-source)
+          username (:username data-source)
+          password (:password data-source)
           ps-key (:ps-key data-source)
           ps-id (:ps-id data-source)
-          ps-point (:excess-power-key data-source)
+          ps-point (:ps-point data-source)
           latest-data-point (get-latest-data-point 
-                              script-filepath 
-                              gosungrow-appkey 
-                              sungrow-username 
-                              sungrow-password 
+                              gosungrow-filepath 
+                              appkey 
+                              username 
+                              password 
                               ps-key 
                               ps-id 
                               ps-point)]
       latest-data-point)))
 
-(defn configure-gosungrow-executable
-  [script-filepath gosungrow-appkey sungrow-username sungrow-password]
-  (let [result (sh 
-                 script-filepath
-                 "config" 
-                 "write" 
-                 (format "--appkey=%s" gosungrow-appkey) 
-                 (format "--user=%s" sungrow-username) 
-                 (format "--password=%s" sungrow-password))]
-    (when (not= 0 (:exit result))
-      (throw (ex-info (format "%s %s" (:err result) (:out result)) {})))))
-
 (defn new-GoSungrowDataSource
-  [script-filepath gosungrow-appkey sungrow-username sungrow-password ps-key ps-id excess-power-key]
-  (let [the-map {:script-filepath script-filepath
-                 :gosungrow-appkey gosungrow-appkey
-                 :sungrow-username sungrow-username
-                 :sungrow-password sungrow-password
+  [gosungrow-filepath appkey username password ps-key ps-id ps-point]
+  (let [the-map {:gosungrow-filepath gosungrow-filepath
+                 :appkey appkey
+                 :username username
+                 :password password
                  :ps-key ps-key
                  :ps-id ps-id
-                 :excess-power-key excess-power-key}
+                 :ps-point ps-point}
         defaults {}]
-    (try
-      (configure-gosungrow-executable
-        script-filepath 
-        gosungrow-appkey 
-        sungrow-username
-        sungrow-password)
-      (catch clojure.lang.ExceptionInfo e
-        (log/error "GoSungrowDataSource" (format "Failed to config GoSungrow executable; %s" (ex-message e))))
-      (catch Exception e
-        (log/error "GoSungrowDataSource" (format "Failed to config GoSungrow executable; %s" (ex-message e)))))
     (map->GoSungrowDataSource (merge defaults the-map))))
 
